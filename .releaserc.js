ddfe34b7202b8ae3eaf351d4ec1dd831e73c980d
Artur Kudlacz <akudlacz@adobe.com>
2020-09-25 18:24:34 +0200

    ci(semantic-release): push `changelog.md` to github on release

--- .releaserc.js
+++ .releaserc.js
@@ -29,7 +29,11 @@ module.exports = {
         'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }
     ],
-    '@semantic-release/git'
+    [
+      '@semantic-release/git', {
+        'assets': [ 'changelog.md' ]
+      }
+    ]
   ],
   branch: 'master',
   branches: [ 'master' ]
